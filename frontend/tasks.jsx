1ed0218db540cdf4ee686d57a24ec18b0afb117e
Arianne Dimoff <ariannedimoff@gmail.com>
2021-08-16 16:22:14 -0700

    create form to add new tasks

--- frontend/tasks.jsx
+++ frontend/tasks.jsx
@@ -11,7 +11,7 @@ class Headers extends React.Component {
         <li
           key={idx}
           id={status}
-          onClick={() => this.props.handleTabClick(idx)}
+          onClick={() => this.props.toggleTab(idx)}
         >
           {header}
         </li>
@@ -21,31 +21,56 @@ class Headers extends React.Component {
   }
 }
 
+class TaskForm extends React.Component {
+
+  inputValue() {
+    return document.getElementById("input").value;
+  }
+  render() {
+    return (
+      <div>
+        <input type="text" 
+        placeholder="Add a new task"
+        id="input"/>
+
+        <button onClick={()=> this.props.addTask(this.inputValue())}>Add</button>
+      </div>
+    );
+  }
+}
+
 class Tasks extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { selectedTab: 0 };
-    this.handleTabClick = this.handleTabClick.bind(this);
+    this.state = { selectedTab: 0, content: "hello" };
+    this.toggleTab = this.toggleTab.bind(this);
+    this.addTask = this.addTask.bind(this);
   }
 
-  handleTabClick(idx) {
+  toggleTab(idx) {
     this.setState({selectedTab: idx})
   };
 
+  addTask(task) {
+    this.setState({content: task})
+  }
+
   render() {
     const tab = this.props.tabs[this.state.selectedTab];
-
+    console.log("STATE", this.state)
     return (
       <div className="tasks card large">
         {/* <h1>My Weekly Tasks</h1> */}
         <div className="tabs">
           <Headers
             selectedTab={this.state.selectedTab}
-            handleTabClick={this.handleTabClick}
+            toggleTab={this.toggleTab}
             tabs={this.props.tabs}
           ></Headers>
           <div className="tab-content">
-            <article>{tab.content}</article>
+            <TaskForm 
+            addTask={this.addTask}/>
+            <article>{this.state.content}</article>
           </div>
         </div>
       </div>
