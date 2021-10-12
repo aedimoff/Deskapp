1adb6254bc6d6b367f0a09048cc0ab356e9616dc
Arianne Dimoff <ariannedimoff@gmail.com>
2021-10-12 16:48:12 -0700

    setup weatherapi

--- frontend/weather.jsx
+++ frontend/weather.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Card } from "react-bootstrap";
 
+
 const Weather = () => {
-  // return <div className="weather card"></div>;
-  return <Card className="weather card">Today's Weather is:</Card>;
+  // const [coords, setCoords] = useState(null)
+  // const coords = navigator.geolocation.getCurrentPosition;
+
+  var options = {
+    method: "GET",
+    url: "https://weatherapi-com.p.rapidapi.com/current.json",
+    params: { q: "q=53.2734, -7.77832031" },
+    headers: {
+      "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+      "x-rapidapi-key": "cea3f490acmshc761a76968cd96cp1b362ejsn0742e741fccd",
+    },
+  };
+
+  const [temp, setTemp] = useState(null);
+  const [weather, setWeather] = useState(null);
+
+  const getWeather = () => {
+    axios.request(options).then(res => {
+      setWeather(res.data.current.condition.text);
+    });
+  }
+  
+  useEffect(() => (
+    getWeather()
+  ), [])
+
+  return (
+    <div>
+      <Card className="weather card">Today's Weather is:{weather}</Card>
+    </div>
+  );
 };
 
 export default Weather;
