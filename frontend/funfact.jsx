d9309a8ad0df56ed69797682246f499c5c6b90b6
Arianne Dimoff <ariannedimoff@gmail.com>
2021-10-12 15:26:45 -0700

    add funfact

--- frontend/funfact.jsx
+++ frontend/funfact.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEfect, useEffect} from "react";
+import axios from "axios";
 
 const FunFact = () => {
-  return <div className="fun-fact card">Today's Fun Fact is:</div>;
+
+  const [fact, setFact] = useState(null)
+
+  const options = {
+    method: "GET",
+    url: "https://facts-by-api-ninjas.p.rapidapi.com/v1/facts",
+    headers: {
+      "x-rapidapi-host": "facts-by-api-ninjas.p.rapidapi.com",
+      "x-rapidapi-key": "cea3f490acmshc761a76968cd96cp1b362ejsn0742e741fccd",
+    },
+  };
+
+
+
+  useEffect(() => {
+      axios.request(options).then((res) => {
+        setFact(res.data[0].fact);
+      });
+  }, [])
+
+
+  return <div className="fun-fact card">Today's Fun Fact is: {fact}</div>;
 };
 
 export default FunFact;
