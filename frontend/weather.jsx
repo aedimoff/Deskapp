21a75f9eda1dad1b826068cac9ca66f05a276bf5
Arianne Dimoff <ariannedimoff@gmail.com>
2021-10-12 17:40:09 -0700

    add weather and geolocation

--- frontend/weather.jsx
+++ frontend/weather.jsx
@@ -1,38 +1,58 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Card } from "react-bootstrap";
-
+import regeneratorRuntime from "regenerator-runtime";
+import { BsSun } from "react-icons/bs";
+import { RiMistLine, RiCloudLine } from "react-icons/ri";
+import { WiRain, WiDayCloudy } from "react-icons/wi";
 
 const Weather = () => {
   // const [coords, setCoords] = useState(null)
   // const coords = navigator.geolocation.getCurrentPosition;
 
-  var options = {
-    method: "GET",
-    url: "https://weatherapi-com.p.rapidapi.com/current.json",
-    params: { q: "q=53.2734, -7.77832031" },
-    headers: {
-      "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
-      "x-rapidapi-key": "cea3f490acmshc761a76968cd96cp1b362ejsn0742e741fccd",
-    },
-  };
-
   const [temp, setTemp] = useState(null);
   const [weather, setWeather] = useState(null);
+  const [location, setLocation] = useState(null);
 
   const getWeather = () => {
-    axios.request(options).then(res => {
-      setWeather(res.data.current.condition.text);
+    navigator.geolocation.getCurrentPosition((pos) => {
+      console.log(pos)
+          const options = {
+            method: "GET",
+            url: "https://weatherapi-com.p.rapidapi.com/current.json",
+            params: { q: `q=${pos.coords.latitude}, ${pos.coords.longitude}` },
+            headers: {
+              "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+              "x-rapidapi-key":
+                "cea3f490acmshc761a76968cd96cp1b362ejsn0742e741fccd",
+            },
+          };
+      axios.request(options).then((res) => {
+        setWeather(res.data.current.condition.text);
+        setTemp(res.data.current.temp_f);
+      });
     });
-  }
-  
-  useEffect(() => (
-    getWeather()
-  ), [])
+  };
+
+
+  const weatherIcons = {
+    "Sunny": <BsSun />,
+    "Clear": <BsSun />,
+    "Mist": <RiMistLine />,
+    "Partly cloudy": <WiDayCloudy />,
+    "Light rain": <WiRain />,
+    "Rain": <WiRain />,
+    "Overcast": <RiCloudLine />,
+  };
+
+  useEffect(() => (getWeather()), []);
 
   return (
     <div>
-      <Card className="weather card">Today's Weather is:{weather}</Card>
+      <Card className="weather card">
+        Today's Weather is:{weather} {temp}
+      </Card>
+      {weatherIcons[weather]}
     </div>
   );
 };
